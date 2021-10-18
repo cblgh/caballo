6943da16189220a077407c8e6fdf7e7537566e12
cblgh <cblgh@cblgh.org>
2021-10-18 17:02:01 +0200

    fix handling of historic messages, refactor debugging info

--- index.js
+++ index.js
@@ -4,11 +4,18 @@ const Client = require("cabal-client")
 const urlParams = new URLSearchParams(window.location.search)
 const useRamStorage = urlParams.get('ram') && urlParams.get('ram').toLowerCase() == "true"
 const RAW = require("random-access-web")
-if (useRamStorage) { console.log("Using RAM storage") }
+if (useRamStorage) { log("Using RAM storage") }
+const DEBUG_STUFF = true
 
 let key = urlParams.get('key') || "13379ad64e284691b7c6f6310e39204b5f92765e36102046caaa6a7ff8c02d74"
 const hyperswarmWebOpts = { bootstrap: ["wss://swarm.cblgh.org"] }
 const client = new Client({ config: { storage: RAW("caballo"+key), swarm: hyperswarmWebOpts, dbdir: "caballo"+key, temp: useRamStorage }})
+
+function log () {
+  if (DEBUG_STUFF) {
+    console.log(...arguments)
+  }
+}
 
 // TODO: expand default bootstrap list to multiple confirmed & reliable nodes
 if (urlParams.get('bootstrap')) {
@@ -17,14 +24,14 @@ if (urlParams.get('bootstrap')) {
 
 client.addCabal(key, { swarm: hyperswarmWebOpts }).then((details) => {
   // start pulling down information :>
-  console.log("cabal is ready")
+  log("cabal is ready")
   initiate(details)
   details.getLocalUser((user) => {
     setName(user.key.slice(0, 8))
   })
 
   details.on("started-peering", (k) => {
-    console.log("new peer", k)
+    log("new peer", k)
   })
 })
 
@@ -39,6 +46,7 @@ function makeMessage(messageText) {
 }
 
 function addMessage (msg) {
+    log("add msg", msg)
     const prev = document.getElementById("chat").textContent 
     // place new messages at the top, so we don't have to attach autoscrolling javascript to the textarea
     document.getElementById("chat").textContent = msg + "\n" + prev 
@@ -49,8 +57,9 @@ function setName (name) {
 }
 
 function initiate (details) {
-  function handleMessage (env) {
-    if (!env || !env.message || !env.message.key) return
+  function handleEnvelope (env) {
+    const isTrash = (!env || !env.message || !env.message.key)
+    if (isTrash) return
     const message = env.message
     let name = message.key.slice(0, 8)
     if (env.author && env.author.name && env.author.name.length < 60) name = env.author.name
@@ -58,10 +67,25 @@ function initiate (details) {
     addMessage(text)
   }
 
-  // load historic data onto page
-  const rs = details._cabal.messages.read("default", { reverse: false })
+  // the read stream api doesn't return the user as part of an envelope object, so we gotta handle those messages separately
+  function handleMessage (msg) {
+    const isTrash = (!msg || !msg.key)
+    if (isTrash) return
+    let name = msg.key.slice(0, 8)
+    // get author info via cabal-client using their pubkey `msg.key`
+    const users = details.getUsers()
+    const user = users[msg.key]
+    log(user)
+    if (user && user.name && user.name.length < 60) name = user.name
+    const text = `[${msg.value.content.channel}] ${name}: ${msg.value.content.text}`
+    addMessage(text)
+  }
+
+  // load historic data onto page; 
+  // only grab 25 messages though in case someone is using caballo as a main client (not recommended!!)
+  const rs = details._cabal.messages.read("default", { reverse: false, limit: 25 })
   rs.on("data", handleMessage) 
-  details.on("new-message", handleMessage)
+  details.on("new-message", handleEnvelope)
   addMessageSendHandler(details)
 }
 
